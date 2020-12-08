0f2fd4857ebb727dbe390d3d1e44859662f9e426
agatakolohe <agatakolohe@gmail.com>
2020-12-08 13:53:10 -0800

    attempt to create ui
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

--- Triangle/Models/Triangle.cs
+++ Triangle/Models/Triangle.cs
@@ -23,5 +23,3 @@ namespace Triangle
     }      
   }
 }
-
-// (side1 > side2 + side3 || side2 > side1 + side3 || side3 > side1 + side2)
--- Triangle/Program.cs
+++ Triangle/Program.cs
@@ -1,21 +1,34 @@
-// //UI
-// using System;
+//UI
+using System;
+using System.Collections.Generic;
+using Triangle;
 
-// // public class Triangle
-// {
-//   public static void AddNumbersumbers()
-//   {
-//    Console.WriteLine
-//   }
+// public class Triangle
 
-  
-// }namespace Calendar
-// {
-//   public class LeapYear
-//   {
-//     public bool IsLeapYear(int year)
-//     {
-//       return year % 4 == 0;
-//     }
-//   }
-// }
+public class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Please pick your first number")
+    string stringSide1 = Console.ReadLine();
+    int side1 = int.Parse(stringSide1);
+
+    Console.WriteLine("Please pick your second number")
+    string stringSide2 = Console.ReadLine();
+    int side2 = int.Parse(stringSide2);
+
+    Console.WriteLine("Please pick your third number")
+    string stringSide3 = Console.ReadLine();
+    int side3 = int.Parse(stringSide3);
+
+    int triangle = //create a triangle constructor
+
+  // public static void AddNumbersumbers()
+  //     {
+  //  Console.WriteLine("Please pick your first number")
+  //  string stringSide1 = Console.ReadLine();
+  //  int side1 = int.Parse(stringSide1);
+  //     }
+    
+  }
+}
